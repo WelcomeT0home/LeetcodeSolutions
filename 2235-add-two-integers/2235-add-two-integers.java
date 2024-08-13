13c7c8e54c7fe143f3b284c93dbd72abf3a585f3
WelcomeT0home <davidowart2000pro@proton.me>
2024-08-13 17:35:11 +0300

    Time: 0 ms (100.00%), Space: 39.8 MB (99.51%) - LeetHub

---
+++ 2235-add-two-integers/2235-add-two-integers.java
@@ -0,0 +1,10 @@
+class Solution {
+    public static int sum(int num1, int num2) {
+       return num1 + num2;
+    }
+    
+    public static void main(String[] args) {
+        int sum = sum(2,4);
+        System.out.println(sum);
+    }
+}
